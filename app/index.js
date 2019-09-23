d0f49bdd1b139305ef9051c21f35e3e10ca0d8e4
paulakedouk <paulakedouk@gmail.com>
2019-09-23 16:06:09 -0700

    Add webpack and run App component

---
+++ app/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                TEST PASSOU!!!!!!!!!!
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('app')
+)
